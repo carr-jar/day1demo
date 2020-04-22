0ff09f4882a2cb1bdb9a7ea7c6b32d1602519a5a
carr-jar <1773298425@qq.com>
2020-04-22 11:35:25 +0800

    把Feign放到接口工程

---
+++ service-user-interface/src/main/java/com/zhuyoupeng/serviceUserInterface/feign/UserFeignClient.java
@@ -0,0 +1,28 @@
+package com.zhuyoupeng.serviceUserInterface.feign;
+
+import com.zhuyoupeng.serviceUserInterface.User;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 指定服务名称
+ * feign客户端调用用户服务接口
+ */
+@FeignClient(name = "service-user")
+public interface UserFeignClient {
+    @RequestMapping("/user/getUserById")
+    public User getUserById(@RequestParam("id") Integer id);
+
+    @RequestMapping("/user/getUserByUser")
+    public User getUserByUser(@RequestBody User user);
+
+    /**
+     * 根据Id，查询用户名称
+     * @param id
+     * @return
+     */
+    @RequestMapping("/user/getUsernameById")
+    public String getUsernameById(@RequestParam("id") Integer id);
+}
