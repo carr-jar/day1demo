d55a913fd2f7bc6f6bc30f2d7f1bfe4f7fa3ca07
carr-jar <1773298425@qq.com>
2020-04-25 10:52:48 +0800

    添加config配置模块及修改配置文件编码

--- cloud-gateway/src/main/java/com/zhuyoupeng/cloudgateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/com/zhuyoupeng/cloudgateway/CloudGatewayApplication.java
@@ -1,8 +1,10 @@
 package com.zhuyoupeng.cloudgateway;
 
+import com.zhuyoupeng.cloudgateway.config.HostAddrKeyResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
